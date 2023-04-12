1a201bcc1f622ad6fe1ded975b816ab50ea00573
Danny <dnnndungu@gmail.com>
2023-04-12 20:03:01 +0300

    Create countryscrape.py

---
+++ countryscrape.py
@@ -0,0 +1,21 @@
+#scrape country, capital, population and size
+from bs4 import BeautifulSoup
+import requests
+
+htmldoc=requests.get('https://www.scrapethissite.com/pages/simple/')
+soup=BeautifulSoup(htmldoc.text,'lxml')
+countries=[]
+stats=[]
+#scraping the country info
+for entry in soup.findAll('div',class_='country-info'):
+    stats.append(entry.text.strip())
+#scraping the country names
+for category in soup.findAll('h3',class_='country-name'):
+    countries.append(category.text.strip())    
+print((countries))
+#getting input for country name to return the country info
+while True:
+    a=input('Enter a country from the choices(case sensitive); ')
+    if a in countries:
+        num=countries.index(a)
+        print(stats[num])
